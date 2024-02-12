830f39b2b9cb64e43ae687e5e419924348784b21
NikitaKogtev <nikitakogtev@gmail.com>
2024-02-12 21:35:26 +0700

    Add datasource and entity (models)

--- src/main/java/ru/kogtev/datasportteam/models/Player.java
+++ src/main/java/ru/kogtev/datasportteam/models/Player.java
@@ -1,4 +1,111 @@
 package ru.kogtev.datasportteam.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Player")
 public class Player {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Имя игрока не должно быть пустым")
+    private String firstName;
+
+    @Column(name = "second_name")
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Фамилия игрока не должно быть пустым")
+    private String secondName;
+
+    @Column(name = "third_name")
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Отчество игрока не должно быть пустым")
+    private String thirdName;
+
+    @Column(name = "birth_date")
+    @NotEmpty
+    @Size(min = 1950, max = 2020, message = "Дата рождения игрока должна быть заполнена по форме 'YYYY' от 1950 до 2020 гг. ")
+    private int birthDate;
+
+    @Column(name = "role")
+    @NotEmpty
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", referencedColumnName = "id")
+    private Team teamOwner;
+
+
+    public Player() {
+    }
+
+    public Player(String firstName, String secondName, String thirdName, int birthDate, String role) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.birthDate = birthDate;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getThirdName() {
+        return thirdName;
+    }
+
+    public void setThirdName(String thirdName) {
+        this.thirdName = thirdName;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Team getTeamOwner() {
+        return teamOwner;
+    }
+
+    public void setTeamOwner(Team teamOwner) {
+        this.teamOwner = teamOwner;
+    }
 }
--- src/main/java/ru/kogtev/datasportteam/models/Team.java
+++ src/main/java/ru/kogtev/datasportteam/models/Team.java
@@ -1,4 +1,86 @@
 package ru.kogtev.datasportteam.models;
 
+import ru.kogtev.datasportteam.util.SportType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Team")
 public class Team {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "team_name")
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Название команды не должно пустым")
+    private String teamName;
+
+    @Column(name = "sport_type")
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Наименование дисциплины не должно быть пустым")
+    private String sportType;
+
+    @Column(name = "found_date")
+    @NotEmpty
+    @Size(min = 1000, max = 2024, message = "Год основания должен быть записан по форме 'YYYY' от 1000 до 2024 гг. ")
+    private int foundDate;
+
+    @OneToMany(mappedBy = "teamOwner")
+    private List<Player> players;
+
+    public Team() {
+    }
+
+    public Team(String teamName, String sportType, int foundDate) {
+        this.teamName = teamName;
+        this.sportType = sportType;
+        this.foundDate = foundDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getSportType() {
+        return sportType;
+    }
+
+    public void setSportType(String sportType) {
+        this.sportType = sportType;
+    }
+
+    public int getFoundDate() {
+        return foundDate;
+    }
+
+    public void setFoundDate(int foundDate) {
+        this.foundDate = foundDate;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 }
